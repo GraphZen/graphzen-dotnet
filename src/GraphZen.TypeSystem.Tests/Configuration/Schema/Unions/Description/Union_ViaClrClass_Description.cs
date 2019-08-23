0b99cb581e1d9e28fbf486f26ea39fe4b3c3f426
Craig Smitham <craig@graphzen.com>
2019-08-23 16:28:47 -0500

    wip

---
+++ src/GraphZen.TypeSystem.Tests/Configuration/Schema/Unions/Description/Union_ViaClrClass_Description.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using GraphZen.Infrastructure;
+using GraphZen.TypeSystem;
+
+namespace GraphZen.Unions.Description
+{
+    public class Union_ViaClrClass_Description : Union_Description, ILeafConventionConfigurationFixture
+    {
+        public const string DataAnnotationDescriptionValue = nameof(DataAnnotationDescriptionValue);
+
+        public LeafConventionContext GetContext() => new LeafConventionContext
+        {
+            ParentName = nameof(ExampleUnion),
+            DataAnnotationValue = DataAnnotationDescriptionValue
+        };
+
+        public void ConfigureContextConventionally(SchemaBuilder sb)
+        {
+            sb.Union<ExampleUnion>();
+        }
+
+        public void ConfigureClrContext(SchemaBuilder sb, string parentName)
+        {
+            sb.Union<ExampleUnion>();
+        }
+
+        [Description(DataAnnotationDescriptionValue)]
+        public class ExampleUnion
+        {
+        }
+    }
+}
--- src/GraphZen.TypeSystem.Tests/Configuration/Schema/Unions/Description/Union_ViaClrMarkerInterface_Description.cs
+++ src/GraphZen.TypeSystem.Tests/Configuration/Schema/Unions/Description/Union_ViaClrMarkerInterface_Description.cs
@@ -4,32 +4,6 @@ using GraphZen.TypeSystem;
 
 namespace GraphZen.Unions.Description
 {
-    public class Union_ViaClrClass_Description : Union_Description, ILeafConventionConfigurationFixture
-    {
-        public const string DataAnnotationDescriptionValue = nameof(DataAnnotationDescriptionValue);
-
-        public LeafConventionContext GetContext() => new LeafConventionContext
-        {
-            ParentName = nameof(ExampleUnion),
-            DataAnnotationValue = DataAnnotationDescriptionValue
-        };
-
-        public void ConfigureContextConventionally(SchemaBuilder sb)
-        {
-            sb.Union<ExampleUnion>();
-        }
-
-        public void ConfigureClrContext(SchemaBuilder sb, string parentName)
-        {
-            sb.Union<ExampleUnion>();
-        }
-
-        [Description(DataAnnotationDescriptionValue)]
-        public class ExampleUnion
-        {
-        }
-    }
-
     public class Union_ViaClrMarkerInterface_Description : Union_Description, ILeafConventionConfigurationFixture
     {
         public const string DataAnnotationDescriptionValue = nameof(DataAnnotationDescriptionValue);
