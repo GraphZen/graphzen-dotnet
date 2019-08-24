8302c0910cb4a860279ca9a5c75ed57581c06e30
Craig Smitham <craig@graphzen.com>
2019-08-23 23:18:56 -0500

    wip

--- src/GraphZen.TypeSystem/GraphQLContextOptionsBuilder.cs
+++ src/GraphZen.TypeSystem/GraphQLContextOptionsBuilder.cs
@@ -1,4 +1,3 @@
-#nullable disable
 // Copyright (c) GraphZen LLC. All rights reserved.
 // Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
 
