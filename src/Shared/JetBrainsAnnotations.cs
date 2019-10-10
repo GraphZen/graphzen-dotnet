64f1fbf77faa34dd731548458a2da0edd6b10d50
Craig Smitham <craig@graphzen.com>
2019-10-10 10:13:34 -0500

    wip

--- src/Shared/JetBrainsAnnotations.cs
+++ src/Shared/JetBrainsAnnotations.cs
@@ -1,6 +1,3 @@
-// Copyright (c) GraphZen LLC. All rights reserved.
-// Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
-
 #nullable disable
 using System;
 using System.Diagnostics.CodeAnalysis;
