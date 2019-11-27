00027097f350dc9a05de587d0d03f8ccd4aa6047
Craig Smitham <craig@graphzen.com>
2019-11-27 17:44:10 -0600

    wip

--- src/GraphZen.Infrastructure.Testing.Tests/UnitTest1.cs
+++ src/GraphZen.Infrastructure.Testing.Tests/UnitTest1.cs
@@ -1,14 +1,18 @@
-using System;
+// Copyright (c) GraphZen LLC. All rights reserved.
+// Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+using GraphZen.Infrastructure;
+using JetBrains.Annotations;
 using Xunit;
 
-namespace GraphZen.Infrastructure.Testing.Tests
+namespace GraphZen.Infrastructure
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-
         }
     }
 }
