e8ca8f67338a457912b05ec1b81a7fe151dce271
Craig Smitham <craig@graphzen.com>
2020-04-06 09:11:48 -0500

    wip

--- BuildTargets/Program.cs
+++ BuildTargets/Program.cs
@@ -1,11 +1,10 @@
 ﻿// Copyright (c) GraphZen LLC. All rights reserved.
 // Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
 
-using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using Palmmedia.ReportGenerator.Core;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
@@ -15,42 +14,33 @@ namespace BuildTargets
     internal class Program
     {
         private const string ArtifactsDir = "build-artifacts";
+
+        private static string OutputDir { get; } =
+            Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName)!;
+
         private static readonly string PackageDir = Path.Combine(ArtifactsDir, "packages");
         private static readonly string TestLogDir = Path.Combine(ArtifactsDir, "test-logs");
-
         private const string Compile = nameof(Compile);
         private const string Default = nameof(Default);
+        private const string Format = nameof(Format);
         private const string Pack = nameof(Pack);
         private const string Test = nameof(Test);
-
+        private const string HtmlReport = nameof(HtmlReport);
 
         private static void Main(string[] args)
         {
             CleanDir($"./{ArtifactsDir}");
             Target(Compile, () => Run("dotnet", "build -c release"));
-            Target(Test, () =>
-            {
-                var testProjects = new DirectoryInfo("./test").GetDirectories().SelectMany(d => d.GetFiles("*.csproj"))
-                        .Take(1)
-                    // .Where(f => true || f.Name.Contains("Client.Tests"))
-                    ;
-                foreach (var testProject in testProjects)
-                {
-                    Console.WriteLine(testProject);
-
-                    //Run("dotnet", $"test {testProject} -c release --no-build --logger trx --results-directory {TestLogDir} --collect:\"XPlat Code Coverage\" --settings:./BuildTargets/coverlet.runsettings.xml");
-                }
-
-                Run("dotnet",
-                    $"test  -c release --no-build --logger trx --results-directory {TestLogDir} --collect:\"XPlat Code Coverage\" --settings:./BuildTargets/coverlet.runsettings.xml");
-                GenerateCodeCoverageReport();
-
-                //Run("dotnet", $"test -c release --no-build --logger trx --results-directory {TestLogDir} /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:ExcludeByAttribute=\"Obsolete, GeneratedCodeAttribute, CompilerGeneratedAttribute\"");
-            });
+            Target(Test, () => GenerateCodeCoverageReport());
+            Target(HtmlReport, () => GenerateCodeCoverageReport(true));
             Target(Pack, DependsOn(Compile), () => Run("dotnet", $"pack -c Release -o ./{PackageDir} --no-build"));
+            Target(Format,
+                () => { Run("cleanupcode", "--config=cleanupcode.config", Path.Combine(OutputDir, "ReSharperTools")); });
             Target(Default, DependsOn(Compile, Test, Pack));
+            // Target();
             RunTargetsAndExit(args);
         }
+
 
         private static void CleanDir(string path)
         {
@@ -58,14 +48,17 @@ namespace BuildTargets
             Directory.CreateDirectory(path);
         }
 
-        private static void GenerateCodeCoverageReport()
+        private static void GenerateCodeCoverageReport(bool html = false)
         {
+            var reportTypes = new List<string>
+            {
+                "Cobertura"
+            };
+            if (html) reportTypes.Add("Html");
             new Generator().GenerateReport(new ReportConfiguration(
-                new List<string> {$"./{TestLogDir}/**/*coverage.cobertura.xml"},
-                $"./{ArtifactsDir}/coverage-report/", new List<string>(), null, new List<string>
-                {
-                    "Cobertura"
-                },
+                new List<string> { $"./{TestLogDir}/**/*coverage.cobertura.xml" },
+                $"./{ArtifactsDir}/coverage-report/", new List<string>(), null,
+                reportTypes,
                 new List<string>(), new List<string>(), new List<string>(), new List<string>(), null,
                 null));
         }
