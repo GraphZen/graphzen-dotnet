6e8874e2fb1c9cb6b1584b1351fc68bf1990adbf
Craig Smitham <craig@graphzen.com>
2020-04-10 18:22:52 -0500

    wip

---
+++ src/GraphZen.TypeSystem/TypeSystem/Field.Generated.cs
@@ -0,0 +1,27 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using GraphZen.Infrastructure;
+using JetBrains.Annotations;
+
+namespace GraphZen.TypeSystem {
+public partial class Field {
+
+        public Argument? FindArgument(String name) 
+            => Arguments.TryGetValue(Check.NotNull(name,nameof(name)), out var argument) ? argument : null;
+
+        public bool HasArgument(String name) 
+            => Arguments.ContainsKey(Check.NotNull(name, nameof(name)));
+        
+        public Argument GetArgument(String name) 
+            => FindArgument(Check.NotNull(name, nameof(name))) ?? throw new Exception($"{this} does not contain a {nameof(Argument)} with name '{name}'.");
+
+
+        public bool TryGetArgument(String name, [NotNullWhen(true)] out Argument? argument)
+             => Arguments.TryGetValue(Check.NotNull(name, nameof(name)), out argument);
+
+}
+}
--- src/Linked/TypeSystem/TypeSystemAccessors.Generated.cs
+++ src/Linked/TypeSystem/TypeSystemAccessors.Generated.cs
@@ -10,46 +10,4 @@ using JetBrains.Annotations;
 #nullable restore
 
 namespace GraphZen.TypeSystem {
-
-
- public static partial class FieldArgumentsAccessorExtensions {
-
-        
-        public static Argument? FindArgument( this Field source, string name) 
-            => source.Arguments.TryGetValue(Check.NotNull(name,nameof(name)), out var nameArgument) ? nameArgument : null;
-
-        public static bool HasArgument( this Field source,  string name) 
-            => source.Arguments.ContainsKey(Check.NotNull(name, nameof(name)));
-
-        
-        public static Argument GetArgument( this Field source,  string name) 
-            => source.FindArgument(Check.NotNull(name, nameof(name))) ?? throw new Exception($"{source} does not contain a argument named '{name}'.");
-
-        public static bool TryGetArgument( this Field source,  string name, [NotNullWhen(true)] out Argument? argument)
-             => source.Arguments.TryGetValue(Check.NotNull(name, nameof(name)), out argument);
-}
- 
-
-
-
-
- public static partial class IArgumentsArgumentsAccessorExtensions {
-
-        
-        public static Argument? FindArgument( this IArguments source, string name) 
-            => source.Arguments.TryGetValue(Check.NotNull(name,nameof(name)), out var nameArgument) ? nameArgument : null;
-
-        public static bool HasArgument( this IArguments source,  string name) 
-            => source.Arguments.ContainsKey(Check.NotNull(name, nameof(name)));
-
-        
-        public static Argument GetArgument( this IArguments source,  string name) 
-            => source.FindArgument(Check.NotNull(name, nameof(name))) ?? throw new Exception($"{source} does not contain a argument named '{name}'.");
-
-        public static bool TryGetArgument( this IArguments source,  string name, [NotNullWhen(true)] out Argument? argument)
-             => source.Arguments.TryGetValue(Check.NotNull(name, nameof(name)), out argument);
-}
- 
-
-
 }
