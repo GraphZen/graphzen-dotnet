3196f3f6cf0ed31361a74251cd47ecc0adb0cd28
Craig Smitham <craig@graphzen.com>
2020-04-20 16:16:30 -0500

    wip

--- test/GraphZen.TypeSystem.FunctionalTests/Directives/DirectivesSpecTests.cs
+++ test/GraphZen.TypeSystem.FunctionalTests/Directives/DirectivesSpecTests.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
 using GraphZen.Infrastructure;
+using JetBrains.Annotations;
 
 namespace GraphZen.TypeSystem.FunctionalTests.Directives
 {
@@ -13,6 +12,5 @@ namespace GraphZen.TypeSystem.FunctionalTests.Directives
     [SpecSubject(nameof(Directive))]
     public abstract class DirectiveSpecTest : DirectivesSpecTests
     {
-
     }
 }
--- test/GraphZen.TypeSystem.FunctionalTests/TypeSystemSpecTests.cs
+++ test/GraphZen.TypeSystem.FunctionalTests/TypeSystemSpecTests.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics.CodeAnalysis;
 using GraphZen.Infrastructure;
+using JetBrains.Annotations;
 
 namespace GraphZen.TypeSystem.FunctionalTests
 {
