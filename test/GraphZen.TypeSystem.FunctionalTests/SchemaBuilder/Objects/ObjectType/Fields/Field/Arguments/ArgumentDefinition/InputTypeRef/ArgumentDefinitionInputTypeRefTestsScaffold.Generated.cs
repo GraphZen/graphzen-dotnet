0a3add3d030d329b10c4bc85900b998b8eb0c3c4
Craig Smitham <craig@graphzen.com>
2020-04-28 15:30:34 -0500

    saving changes before code-gen

---
+++ test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/Objects/ObjectType/Fields/Field/Arguments/ArgumentDefinition/InputTypeRef/ArgumentDefinitionInputTypeRefTestsScaffold.Generated.cs
@@ -0,0 +1,45 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using GraphZen.Infrastructure;
+using JetBrains.Annotations;
+
+using Xunit;
+using static GraphZen.TypeSystem.FunctionalTests.Specs.TypeSystemSpecs;
+// ReSharper disable PartialTypeWithSinglePart
+namespace GraphZen.TypeSystem.FunctionalTests.SchemaBuilder.Objects.ObjectType.Fields.Field.Arguments.ArgumentDefinition.InputTypeRef {
+[NoReorder]
+public  class ArgumentDefinitionInputTypeRefTests {
+
+// Priority: Low
+// Subject Name: InputTypeRef
+[Spec(nameof(UpdateableSpecs.it_can_be_updated))]
+[Fact(Skip = "generated")]
+public void it_can_be_updated() {
+    var schema = Schema.Create(_ => {
+
+    });
+}
+
+
+
+// Priority: Low
+// Subject Name: InputTypeRef
+[Spec(nameof(RequiredSpecs.required_item_cannot_be_removed))]
+[Fact(Skip = "generated")]
+public void required_item_cannot_be_removed() {
+    var schema = Schema.Create(_ => {
+
+    });
+}
+
+
+}
+// Move ArgumentDefinitionInputTypeRefTests into a separate file to start writing tests
+[NoReorder] 
+public  class ArgumentDefinitionInputTypeRefTestsScaffold {
+}
+}
---
+++ test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/Objects/ObjectType/Interfaces/ObjectTypeInterfacesTestsScaffold.Generated.cs
@@ -0,0 +1,57 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using GraphZen.Infrastructure;
+using JetBrains.Annotations;
+
+using Xunit;
+using static GraphZen.TypeSystem.FunctionalTests.Specs.TypeSystemSpecs;
+// ReSharper disable PartialTypeWithSinglePart
+namespace GraphZen.TypeSystem.FunctionalTests.SchemaBuilder.Objects.ObjectType.Interfaces {
+[NoReorder]
+public  class ObjectTypeInterfacesTests {
+
+// Priority: Low
+// Subject Name: Interfaces
+[Spec(nameof(NamedTypeSetSpecs.set_item_can_be_added))]
+[Fact(Skip = "generated")]
+public void set_item_can_be_added() {
+    var schema = Schema.Create(_ => {
+
+    });
+}
+
+
+
+// Priority: Low
+// Subject Name: Interfaces
+[Spec(nameof(NamedTypeSetSpecs.set_item_can_be_removed))]
+[Fact(Skip = "generated")]
+public void set_item_can_be_removed() {
+    var schema = Schema.Create(_ => {
+
+    });
+}
+
+
+
+// Priority: Low
+// Subject Name: Interfaces
+[Spec(nameof(NamedTypeSetSpecs.set_item_must_be_valid_name))]
+[Fact(Skip = "generated")]
+public void set_item_must_be_valid_name() {
+    var schema = Schema.Create(_ => {
+
+    });
+}
+
+
+}
+// Move ObjectTypeInterfacesTests into a separate file to start writing tests
+[NoReorder] 
+public  class ObjectTypeInterfacesTestsScaffold {
+}
+}
