470b3cbfca5972975656e7e67d3978e46bc274de
Craig Smitham <craig@graphzen.com>
2020-04-28 15:48:53 -0500

    saving changes before code-gen

--- test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/DirectiveAnnotations/SchemaBuilderDirectiveAnnotationsTests.cs
+++ test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/DirectiveAnnotations/SchemaBuilderDirectiveAnnotationsTests.cs
@@ -24,52 +24,6 @@ namespace GraphZen.TypeSystem.FunctionalTests.SchemaBuilder.DirectiveAnnotations
 
 
 
-// Priority: Low
-// Subject Name: DirectiveAnnotations
-        [Spec(nameof(TypeSystemSpecs.NamedCollectionSpecs.named_item_can_be_removed))]
-        [Fact(Skip = "generated")]
-        public void named_item_can_be_removed() {
-            var schema = Schema.Create(_ => {
-
-            });
-        }
-
-
-
-// Priority: Low
-// Subject Name: DirectiveAnnotations
-        [Spec(nameof(TypeSystemSpecs.NamedCollectionSpecs.named_item_can_be_renamed))]
-        [Fact(Skip = "generated")]
-        public void named_item_can_be_renamed() {
-            var schema = Schema.Create(_ => {
-
-            });
-        }
-
-
-
-// Priority: Low
-// Subject Name: DirectiveAnnotations
-        [Spec(nameof(TypeSystemSpecs.NamedCollectionSpecs.named_item_cannot_be_renamed_if_name_already_exists))]
-        [Fact(Skip = "generated")]
-        public void named_item_cannot_be_renamed_if_name_already_exists() {
-            var schema = Schema.Create(_ => {
-
-            });
-        }
-
-
-
-// Priority: Low
-// Subject Name: DirectiveAnnotations
-        [Spec(nameof(TypeSystemSpecs.NamedCollectionSpecs.named_item_name_must_be_valid_name))]
-        [Fact(Skip = "generated")]
-        public void named_item_name_must_be_valid_name() {
-            var schema = Schema.Create(_ => {
-
-            });
-        }
-
 
     }
 }
