6c6ad23c9ffbe9b0661e0b999eaa37593e78bae1
Craig Smitham <craig@graphzen.com>
2020-04-28 20:18:55 -0500

    saving changes before code-gen

---
+++ test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/DirectiveAnnotations/SchemaBuilderDirectiveAnnotationsTestsScaffold.Generated.cs
@@ -0,0 +1,80 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using GraphZen.Infrastructure;
+using JetBrains.Annotations;
+
+using Xunit;
+using static GraphZen.TypeSystem.FunctionalTests.Specs.TypeSystemSpecs;
+namespace GraphZen.TypeSystem.FunctionalTests.SchemaBuilder.DirectiveAnnotations {
+[NoReorder]
+public abstract  class SchemaBuilderDirectiveAnnotationsTests {
+
+[Spec(nameof(NamedCollectionSpecs.named_item_can_be_added))]
+[Fact]
+public void named_item_can_be_added() {
+    // Priority: Low
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(NamedCollectionSpecs.named_item_can_be_removed))]
+[Fact]
+public void named_item_can_be_removed() {
+    // Priority: Low
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(NamedCollectionSpecs.named_item_can_be_renamed))]
+[Fact]
+public void named_item_can_be_renamed() {
+    // Priority: Low
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(NamedCollectionSpecs.named_item_cannot_be_renamed_if_name_already_exists))]
+[Fact]
+public void named_item_cannot_be_renamed_if_name_already_exists() {
+    // Priority: Low
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(NamedCollectionSpecs.named_item_name_must_be_valid_name))]
+[Fact]
+public void named_item_name_must_be_valid_name() {
+    // Priority: Low
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+}
+// Move SchemaBuilderDirectiveAnnotationsTests into a separate file to start writing tests
+[NoReorder] 
+public  class SchemaBuilderDirectiveAnnotationsTestsScaffold {
+}
+}
