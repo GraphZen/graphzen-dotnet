791764149a49c7620c6df5f37f15c648e1ec79e5
Craig Smitham <craig@graphzen.com>
2020-04-29 11:08:03 -0500

    saving changes before code-gen

--- src/GraphZen.DevCli.Tests/SpecFx/SpecTests.cs
+++ src/GraphZen.DevCli.Tests/SpecFx/SpecTests.cs
@@ -1,17 +1,14 @@
 ﻿// Copyright (c) GraphZen LLC. All rights reserved.
 // Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
 
-using System;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
 using GraphZen.Infrastructure;
 using GraphZen.SpecAudit;
 using GraphZen.SpecAudit.SpecFx;
 using JetBrains.Annotations;
 using Xunit;
 
-namespace GraphZen
+namespace GraphZen.SpecFx
 {
     public class SpecTests
     {
