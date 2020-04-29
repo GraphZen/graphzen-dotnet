d3b197472d4136aa4762121268fd39aea805d835
Craig Smitham <craig@graphzen.com>
2020-04-29 17:46:28 -0500

    saving changes before code-gen

--- src/GraphZen.DevCli/CodeGen/Generators/SchemaBuilderDefaultContextGenerator.cs
+++ src/GraphZen.DevCli/CodeGen/Generators/SchemaBuilderDefaultContextGenerator.cs
@@ -1,6 +1,7 @@
 // Copyright (c) GraphZen LLC. All rights reserved.
 // Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
@@ -15,10 +16,14 @@ using JetBrains.Annotations;
 
 namespace GraphZen.CodeGen
 {
-    public class SchemaBuilderDefaultContextGenerator : PartialTypeGenerator<SchemaBuilder>
+    public class SchemaBuilderDefaultContextGenerator : PartialTypeGenerator
     {
         public override IReadOnlyList<string> Usings { get; } =
             ImmutableList.Create("GraphZen.TypeSystem.Internal");
+
+        public SchemaBuilderDefaultContextGenerator() : base(typeof(SchemaBuilder<>))
+        {
+        }
 
         public override void Apply(StringBuilder csharp)
         {
@@ -61,22 +66,22 @@ public  I{config.TypeName}Builder<{config.DefaultTypeName}> {kind}(Type clrType)
                     else if (config.ContextBuilder)
                     {
                         region.AppendLine($@"
-public I{config.TypeName}Builder<{config.DefaultTypeName}, GraphQLContext> {kind}(string name) {{
+public I{config.TypeName}Builder<{config.DefaultTypeName}, TContext> {kind}(string name) {{
     Check.NotNull(name, nameof(name));
     var internalBuilder = Builder.{kind}(name, ConfigurationSource.Explicit)!;
-    var builder = new {config.TypeName}Builder<{config.DefaultTypeName}, GraphQLContext>(internalBuilder);
+    var builder = new {config.TypeName}Builder<{config.DefaultTypeName}, TContext>(internalBuilder);
     return builder;
 }} 
 
-public  I{config.TypeName}Builder<{typeParam}, GraphQLContext> {kind}<{typeParam}>() where {typeParam} : notnull {{
+public  I{config.TypeName}Builder<{typeParam}, TContext> {kind}<{typeParam}>() where {typeParam} : notnull {{
     var internalBuilder = Builder.{kind}(typeof({typeParam}), ConfigurationSource.Explicit)!;
-    var builder = new {config.TypeName}Builder<{typeParam}, GraphQLContext>(internalBuilder);
+    var builder = new {config.TypeName}Builder<{typeParam}, TContext>(internalBuilder);
     return builder;
 }}
-public  I{config.TypeName}Builder<{config.DefaultTypeName}, GraphQLContext> {kind}(Type clrType)  {{
+public  I{config.TypeName}Builder<{config.DefaultTypeName}, TContext> {kind}(Type clrType)  {{
             Check.NotNull(clrType, nameof(clrType));
     var internalBuilder = Builder.{kind}(clrType, ConfigurationSource.Explicit)!;
-    var builder = new {config.TypeName}Builder<{config.DefaultTypeName}, GraphQLContext>(internalBuilder);
+    var builder = new {config.TypeName}Builder<{config.DefaultTypeName}, TContext>(internalBuilder);
     return builder;
 }}
 
@@ -91,36 +96,36 @@ public  I{config.TypeName}Builder<{config.DefaultTypeName}, GraphQLContext> {kin
                     region.AppendLine($@"
 
 
-public ISchemaBuilder<GraphQLContext> Unignore{kind}<{typeParam}>() where {typeParam}: notnull {{
+public ISchemaBuilder<TContext> Unignore{kind}<{typeParam}>() where {typeParam}: notnull {{
     Builder.Unignore{kind}(typeof({typeParam}), ConfigurationSource.Explicit);
     return this;
 }}
 
-public ISchemaBuilder<GraphQLContext> Unignore{kind}(Type clrType) {{
+public ISchemaBuilder<TContext> Unignore{kind}(Type clrType) {{
     Check.NotNull(clrType, nameof(clrType));
     Builder.Unignore{kind}(clrType, ConfigurationSource.Explicit);
     return this;
 }}
 
-public ISchemaBuilder<GraphQLContext> Unignore{kind}(string name) {{
+public ISchemaBuilder<TContext> Unignore{kind}(string name) {{
     Check.NotNull(name, nameof(name));
     Builder.Unignore{kind}(name, ConfigurationSource.Explicit);
     return this;
 }}
 
 
-public ISchemaBuilder<GraphQLContext> Ignore{kind}<{typeParam}>() where {typeParam}: notnull {{
+public ISchemaBuilder<TContext> Ignore{kind}<{typeParam}>() where {typeParam}: notnull {{
     Builder.Ignore{kind}(typeof({typeParam}), ConfigurationSource.Explicit);
     return this;
 }}
 
-public ISchemaBuilder<GraphQLContext> Ignore{kind}(Type clrType) {{
+public ISchemaBuilder<TContext> Ignore{kind}(Type clrType) {{
     Check.NotNull(clrType, nameof(clrType));
     Builder.Ignore{kind}(clrType, ConfigurationSource.Explicit);
     return this;
 }}
 
-public ISchemaBuilder<GraphQLContext> Ignore{kind}(string name) {{
+public ISchemaBuilder<TContext> Ignore{kind}(string name) {{
     Check.NotNull(name, nameof(name));
     Builder.Ignore{kind}(name, ConfigurationSource.Explicit);
     return this;
@@ -131,5 +136,7 @@ public ISchemaBuilder<GraphQLContext> Ignore{kind}(string name) {{
                 });
             }
         }
+
+
     }
 }
