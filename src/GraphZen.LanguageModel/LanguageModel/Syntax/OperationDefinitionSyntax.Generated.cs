1c0b8938cb2dc49c9721f88da974e9e915336788
Craig Smitham <craig@graphzen.com>
2020-05-03 14:49:36 -0500

    wip

--- src/GraphZen.LanguageModel/LanguageModel/Syntax/OperationDefinitionSyntax.Generated.cs
+++ src/GraphZen.LanguageModel/LanguageModel/Syntax/OperationDefinitionSyntax.Generated.cs
@@ -1,8 +1,6 @@
-// Copyright (c) GraphZen LLC. All rights reserved.
-// Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
-
 #nullable enable
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
@@ -12,34 +10,20 @@ using JetBrains.Annotations;
 
 // ReSharper disable InconsistentNaming
 
-namespace GraphZen.LanguageModel
-{
-    public partial class OperationDefinitionSyntax
-    {
-        /// <summary>Empty, read-only list of <see cref="OperationDefinitionSyntax" /> nodes.</summary>
-        public static IReadOnlyList<OperationDefinitionSyntax> EmptyList { get; } =
-            ImmutableList<OperationDefinitionSyntax>.Empty;
+namespace GraphZen.LanguageModel {
+public  partial class OperationDefinitionSyntax {
 
-        /// <summary>Called when a <see cref="GraphQLSyntaxVisitor" /> enters a <see cref="OperationDefinitionSyntax" /> node.</summary>
-        public override void VisitEnter(GraphQLSyntaxVisitor visitor) => visitor.EnterOperationDefinition(this);
+	    /// <summary>Empty, read-only list of <see cref="OperationDefinitionSyntax"/> nodes.</summary>
+		public static IReadOnlyList<OperationDefinitionSyntax> EmptyList {get;} = ImmutableList<OperationDefinitionSyntax>.Empty; 
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor"/> enters a <see cref="OperationDefinitionSyntax"/> node.</summary>
+		public override void VisitEnter( GraphQLSyntaxVisitor visitor) => visitor.EnterOperationDefinition(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor"/> leaves a <see cref="OperationDefinitionSyntax"/> node.</summary>
+		public override void VisitLeave( GraphQLSyntaxVisitor visitor) => visitor.LeaveOperationDefinition(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor{TResult}"/> enters a <see cref="OperationDefinitionSyntax"/> node.</summary>
+		public override TResult VisitEnter<TResult>( GraphQLSyntaxVisitor<TResult> visitor) => visitor.EnterOperationDefinition(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor{TResult}"/> leaves a <see cref="OperationDefinitionSyntax"/> node.</summary>
+		public override TResult VisitLeave<TResult>( GraphQLSyntaxVisitor<TResult> visitor) => visitor.LeaveOperationDefinition(this);
+		public override SyntaxKind Kind {get;} = SyntaxKind.OperationDefinition;	
 
-        /// <summary>Called when a <see cref="GraphQLSyntaxVisitor" /> leaves a <see cref="OperationDefinitionSyntax" /> node.</summary>
-        public override void VisitLeave(GraphQLSyntaxVisitor visitor) => visitor.LeaveOperationDefinition(this);
-
-        /// <summary>
-        ///     Called when a <see cref="GraphQLSyntaxVisitor{TResult}" /> enters a <see cref="OperationDefinitionSyntax" />
-        ///     node.
-        /// </summary>
-        public override TResult VisitEnter<TResult>(GraphQLSyntaxVisitor<TResult> visitor) =>
-            visitor.EnterOperationDefinition(this);
-
-        /// <summary>
-        ///     Called when a <see cref="GraphQLSyntaxVisitor{TResult}" /> leaves a <see cref="OperationDefinitionSyntax" />
-        ///     node.
-        /// </summary>
-        public override TResult VisitLeave<TResult>(GraphQLSyntaxVisitor<TResult> visitor) =>
-            visitor.LeaveOperationDefinition(this);
-
-        public override SyntaxKind Kind { get; } = SyntaxKind.OperationDefinition;
-    }
+}
 }
--- test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/Objects/ObjectType/DirectiveAnnotations/DirectiveAnnotationsTestsScaffold.Generated.cs
+++ test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/Objects/ObjectType/DirectiveAnnotations/DirectiveAnnotationsTestsScaffold.Generated.cs
@@ -1,163 +1,189 @@
-// Copyright (c) GraphZen LLC. All rights reserved.
-// Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
-
 #nullable enable
 
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using GraphZen.Infrastructure;
 using JetBrains.Annotations;
+
 using Xunit;
 using static GraphZen.TypeSystem.FunctionalTests.Specs.TypeSystemSpecs;
+// ReSharper disable All
+namespace GraphZen.TypeSystem.FunctionalTests.SchemaBuilder.Objects.ObjectType.DirectiveAnnotations {
+[NoReorder]
+public abstract  class DirectiveAnnotationsTests {
 
-namespace GraphZen.TypeSystem.FunctionalTests.SchemaBuilder.Objects.ObjectType.DirectiveAnnotations
-{
-    [NoReorder]
-    public abstract class DirectiveAnnotationsTests
-    {
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_can_be_added))]
-        [Fact]
-        public void directive_annotation_can_be_added()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_can_be_added))]
+[Fact]
+public void directive_annotation_can_be_added() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
 
-
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_added_unless_directive_is_defined))]
-        [Fact]
-        public void directive_annotation_cannot_be_added_unless_directive_is_defined()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_added_unless_location_is_valid))]
-        [Fact]
-        public void directive_annotation_cannot_be_added_unless_location_is_valid()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_added_unless_directive_is_defined))]
+[Fact]
+public void directive_annotation_cannot_be_added_unless_directive_is_defined() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_added_with_invalid_name))]
-        [Fact]
-        public void directive_annotation_cannot_be_added_with_invalid_name()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_added_unless_location_is_valid))]
+[Fact]
+public void directive_annotation_cannot_be_added_unless_location_is_valid() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_added_with_null_name))]
-        [Fact]
-        public void directive_annotation_cannot_be_added_with_null_name()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_added_with_invalid_name))]
+[Fact]
+public void directive_annotation_cannot_be_added_with_invalid_name() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_upserted_unless_directive_is_defined))]
-        [Fact]
-        public void directive_annotation_cannot_be_upserted_unless_directive_is_defined()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_added_with_null_name))]
+[Fact]
+public void directive_annotation_cannot_be_added_with_null_name() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_upserted_unless_location_is_valid))]
-        [Fact]
-        public void directive_annotation_cannot_be_upserted_unless_location_is_valid()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_upserted_unless_directive_is_defined))]
+[Fact]
+public void directive_annotation_cannot_be_upserted_unless_directive_is_defined() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_upserted_with_invalid_name))]
-        [Fact]
-        public void directive_annotation_cannot_be_upserted_with_invalid_name()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_upserted_unless_location_is_valid))]
+[Fact]
+public void directive_annotation_cannot_be_upserted_unless_location_is_valid() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_upserted_with_null_name))]
-        [Fact]
-        public void directive_annotation_cannot_be_upserted_with_null_name()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_upserted_with_invalid_name))]
+[Fact]
+public void directive_annotation_cannot_be_upserted_with_invalid_name() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_are_removed_when_directive_is_ignored))]
-        [Fact]
-        public void directive_annotations_are_removed_when_directive_is_ignored()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotation_cannot_be_upserted_with_null_name))]
+[Fact]
+public void directive_annotation_cannot_be_upserted_with_null_name() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_are_removed_when_directive_is_removed))]
-        [Fact]
-        public void directive_annotations_are_removed_when_directive_is_removed()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_are_removed_when_directive_is_ignored))]
+[Fact]
+public void directive_annotations_are_removed_when_directive_is_ignored() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_can_be_removed))]
-        [Fact]
-        public void directive_annotations_can_be_removed()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_are_removed_when_directive_is_removed))]
+[Fact]
+public void directive_annotations_are_removed_when_directive_is_removed() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_can_be_removed_by_name))]
-        [Fact]
-        public void directive_annotations_can_be_removed_by_name()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_can_be_removed))]
+[Fact]
+public void directive_annotations_can_be_removed() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
 
 
-        [Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_cannot_be_removed_by_name_with_null_name))]
-        [Fact]
-        public void directive_annotations_cannot_be_removed_by_name_with_null_name()
-        {
-            // Priority: High
-            var schema = Schema.Create(_ => { });
-            throw new NotImplementedException();
-        }
-    }
 
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_can_be_removed_by_name))]
+[Fact]
+public void directive_annotations_can_be_removed_by_name() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(DirectiveAnnotationSpecs.directive_annotations_cannot_be_removed_by_name_with_null_name))]
+[Fact]
+public void directive_annotations_cannot_be_removed_by_name_with_null_name() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+}
 // Move DirectiveAnnotationsTests into a separate file to start writing tests
-    [NoReorder]
-    public class DirectiveAnnotationsTestsScaffold
-    {
-    }
+[NoReorder] 
+public  class DirectiveAnnotationsTestsScaffold {
+}
 }
