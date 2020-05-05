1a887301339db3c390ffe54c41e72ff24e336741
Craig Smitham <craig@graphzen.com>
2020-05-05 12:28:40 -0500

    saving changes before code-gen

---
+++ test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/Objects/ObjectType/Fields/Field/DirectiveAnnotations/DirectiveAnnotation/Name/NameTestsScaffold.Generated.cs
@@ -0,0 +1,69 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using GraphZen.Infrastructure;
+using JetBrains.Annotations;
+
+using Xunit;
+using static GraphZen.TypeSystem.FunctionalTests.Specs.TypeSystemSpecs;
+// ReSharper disable All
+namespace GraphZen.TypeSystem.FunctionalTests.SchemaBuilder.Objects.ObjectType.Fields.Field.DirectiveAnnotations.DirectiveAnnotation.Name {
+[NoReorder]
+public abstract  class NameTests {
+
+[Spec(nameof(SdlSpec.element_can_be_defined_via_sdl))]
+[Fact]
+public void element_can_be_defined_via_sdl() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(SdlSpec.element_can_be_defined_via_sdl_extension))]
+[Fact]
+public void element_can_be_defined_via_sdl_extension() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(RequiredSpecs.required_item_cannot_be_removed))]
+[Fact]
+public void required_item_cannot_be_removed() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(UpdateableSpecs.updateable_item_can_be_updated))]
+[Fact]
+public void updateable_item_can_be_updated() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+}
+// Move NameTests into a separate file to start writing tests
+[NoReorder] 
+public  class NameTestsScaffold {
+}
+}
---
+++ test/GraphZen.TypeSystem.FunctionalTests/SchemaBuilder/Objects/ObjectType/Interfaces/InterfacesTestsScaffold.Generated.cs
@@ -0,0 +1,57 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using GraphZen.Infrastructure;
+using JetBrains.Annotations;
+
+using Xunit;
+using static GraphZen.TypeSystem.FunctionalTests.Specs.TypeSystemSpecs;
+// ReSharper disable All
+namespace GraphZen.TypeSystem.FunctionalTests.SchemaBuilder.Objects.ObjectType.Interfaces {
+[NoReorder]
+public abstract  class InterfacesTests {
+
+[Spec(nameof(NamedTypeSetSpecs.set_item_can_be_added))]
+[Fact]
+public void set_item_can_be_added() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(NamedTypeSetSpecs.set_item_can_be_removed))]
+[Fact]
+public void set_item_can_be_removed() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+
+[Spec(nameof(NamedTypeSetSpecs.set_item_must_be_valid_name))]
+[Fact]
+public void set_item_must_be_valid_name() {
+    // Priority: High
+    var schema = Schema.Create(_ => {
+
+    });
+    throw new NotImplementedException();
+}
+
+
+}
+// Move InterfacesTests into a separate file to start writing tests
+[NoReorder] 
+public  class InterfacesTestsScaffold {
+}
+}
