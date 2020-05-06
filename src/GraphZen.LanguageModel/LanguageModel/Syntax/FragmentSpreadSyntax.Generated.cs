391a03b04c6b6e5451f8e6f11a312992bae8f15a
Craig Smitham <craig@graphzen.com>
2020-05-06 08:13:50 -0500

    saving changes before code-gen

--- src/GraphZen.LanguageModel/LanguageModel/Syntax/FragmentSpreadSyntax.Generated.cs
+++ src/GraphZen.LanguageModel/LanguageModel/Syntax/FragmentSpreadSyntax.Generated.cs
@@ -1,8 +1,6 @@
-// Copyright (c) GraphZen LLC. All rights reserved.
-// Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
-
 #nullable enable
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
@@ -11,29 +9,22 @@ using JetBrains.Annotations;
 
 
 // ReSharper disable InconsistentNaming
+// ReSharper disable once PossibleInterfaceMemberAmbiguity
 
-namespace GraphZen.LanguageModel
-{
-    public partial class FragmentSpreadSyntax
-    {
-        /// <summary>Empty, read-only list of <see cref="FragmentSpreadSyntax" /> nodes.</summary>
-        public static IReadOnlyList<FragmentSpreadSyntax> EmptyList { get; } =
-            ImmutableList<FragmentSpreadSyntax>.Empty;
+namespace GraphZen.LanguageModel {
+public  partial class FragmentSpreadSyntax {
 
-        /// <summary>Called when a <see cref="GraphQLSyntaxVisitor" /> enters a <see cref="FragmentSpreadSyntax" /> node.</summary>
-        public override void VisitEnter(GraphQLSyntaxVisitor visitor) => visitor.EnterFragmentSpread(this);
+	    /// <summary>Empty, read-only list of <see cref="FragmentSpreadSyntax"/> nodes.</summary>
+		public static IReadOnlyList<FragmentSpreadSyntax> EmptyList {get;} = ImmutableList<FragmentSpreadSyntax>.Empty; 
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor"/> enters a <see cref="FragmentSpreadSyntax"/> node.</summary>
+		public override void VisitEnter( GraphQLSyntaxVisitor visitor) => visitor.EnterFragmentSpread(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor"/> leaves a <see cref="FragmentSpreadSyntax"/> node.</summary>
+		public override void VisitLeave( GraphQLSyntaxVisitor visitor) => visitor.LeaveFragmentSpread(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor{TResult}"/> enters a <see cref="FragmentSpreadSyntax"/> node.</summary>
+		public override TResult VisitEnter<TResult>( GraphQLSyntaxVisitor<TResult> visitor) => visitor.EnterFragmentSpread(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor{TResult}"/> leaves a <see cref="FragmentSpreadSyntax"/> node.</summary>
+		public override TResult VisitLeave<TResult>( GraphQLSyntaxVisitor<TResult> visitor) => visitor.LeaveFragmentSpread(this);
+		public override SyntaxKind Kind {get;} = SyntaxKind.FragmentSpread;	
 
-        /// <summary>Called when a <see cref="GraphQLSyntaxVisitor" /> leaves a <see cref="FragmentSpreadSyntax" /> node.</summary>
-        public override void VisitLeave(GraphQLSyntaxVisitor visitor) => visitor.LeaveFragmentSpread(this);
-
-        /// <summary>Called when a <see cref="GraphQLSyntaxVisitor{TResult}" /> enters a <see cref="FragmentSpreadSyntax" /> node.</summary>
-        public override TResult VisitEnter<TResult>(GraphQLSyntaxVisitor<TResult> visitor) =>
-            visitor.EnterFragmentSpread(this);
-
-        /// <summary>Called when a <see cref="GraphQLSyntaxVisitor{TResult}" /> leaves a <see cref="FragmentSpreadSyntax" /> node.</summary>
-        public override TResult VisitLeave<TResult>(GraphQLSyntaxVisitor<TResult> visitor) =>
-            visitor.LeaveFragmentSpread(this);
-
-        public override SyntaxKind Kind { get; } = SyntaxKind.FragmentSpread;
-    }
+}
 }
--- src/GraphZen.LanguageModel/LanguageModel/Syntax/InputObjectTypeExtensionSyntax.Generated.cs
+++ src/GraphZen.LanguageModel/LanguageModel/Syntax/InputObjectTypeExtensionSyntax.Generated.cs
@@ -1,8 +1,6 @@
-// Copyright (c) GraphZen LLC. All rights reserved.
-// Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
-
 #nullable enable
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
@@ -11,35 +9,22 @@ using JetBrains.Annotations;
 
 
 // ReSharper disable InconsistentNaming
+// ReSharper disable once PossibleInterfaceMemberAmbiguity
 
-namespace GraphZen.LanguageModel
-{
-    public partial class InputObjectTypeExtensionSyntax
-    {
-        /// <summary>Empty, read-only list of <see cref="InputObjectTypeExtensionSyntax" /> nodes.</summary>
-        public static IReadOnlyList<InputObjectTypeExtensionSyntax> EmptyList { get; } =
-            ImmutableList<InputObjectTypeExtensionSyntax>.Empty;
+namespace GraphZen.LanguageModel {
+public  partial class InputObjectTypeExtensionSyntax {
 
-        /// <summary>Called when a <see cref="GraphQLSyntaxVisitor" /> enters a <see cref="InputObjectTypeExtensionSyntax" /> node.</summary>
-        public override void VisitEnter(GraphQLSyntaxVisitor visitor) => visitor.EnterInputObjectTypeExtension(this);
+	    /// <summary>Empty, read-only list of <see cref="InputObjectTypeExtensionSyntax"/> nodes.</summary>
+		public static IReadOnlyList<InputObjectTypeExtensionSyntax> EmptyList {get;} = ImmutableList<InputObjectTypeExtensionSyntax>.Empty; 
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor"/> enters a <see cref="InputObjectTypeExtensionSyntax"/> node.</summary>
+		public override void VisitEnter( GraphQLSyntaxVisitor visitor) => visitor.EnterInputObjectTypeExtension(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor"/> leaves a <see cref="InputObjectTypeExtensionSyntax"/> node.</summary>
+		public override void VisitLeave( GraphQLSyntaxVisitor visitor) => visitor.LeaveInputObjectTypeExtension(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor{TResult}"/> enters a <see cref="InputObjectTypeExtensionSyntax"/> node.</summary>
+		public override TResult VisitEnter<TResult>( GraphQLSyntaxVisitor<TResult> visitor) => visitor.EnterInputObjectTypeExtension(this);
+		/// <summary>Called when a <see cref="GraphQLSyntaxVisitor{TResult}"/> leaves a <see cref="InputObjectTypeExtensionSyntax"/> node.</summary>
+		public override TResult VisitLeave<TResult>( GraphQLSyntaxVisitor<TResult> visitor) => visitor.LeaveInputObjectTypeExtension(this);
+		public override SyntaxKind Kind {get;} = SyntaxKind.InputObjectTypeExtension;	
 
-        /// <summary>Called when a <see cref="GraphQLSyntaxVisitor" /> leaves a <see cref="InputObjectTypeExtensionSyntax" /> node.</summary>
-        public override void VisitLeave(GraphQLSyntaxVisitor visitor) => visitor.LeaveInputObjectTypeExtension(this);
-
-        /// <summary>
-        ///     Called when a <see cref="GraphQLSyntaxVisitor{TResult}" /> enters a
-        ///     <see cref="InputObjectTypeExtensionSyntax" /> node.
-        /// </summary>
-        public override TResult VisitEnter<TResult>(GraphQLSyntaxVisitor<TResult> visitor) =>
-            visitor.EnterInputObjectTypeExtension(this);
-
-        /// <summary>
-        ///     Called when a <see cref="GraphQLSyntaxVisitor{TResult}" /> leaves a
-        ///     <see cref="InputObjectTypeExtensionSyntax" /> node.
-        /// </summary>
-        public override TResult VisitLeave<TResult>(GraphQLSyntaxVisitor<TResult> visitor) =>
-            visitor.LeaveInputObjectTypeExtension(this);
-
-        public override SyntaxKind Kind { get; } = SyntaxKind.InputObjectTypeExtension;
-    }
+}
 }
