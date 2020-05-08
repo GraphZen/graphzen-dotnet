653d9b79fc8d59d23634f83226c611f63d7a695e
Craig Smitham <craig@graphzen.com>
2020-05-08 17:12:34 -0500

    saving changes before code-gen

--- BuildTargets/Program.cs
+++ BuildTargets/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) GraphZen LLC. All rights reserved.
 // Licensed under the GraphZen Community License. See the LICENSE file in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
@@ -137,6 +138,7 @@ namespace BuildTargets
                 {
                     CleanupCode(modified);
                 }
+                
             }
         }
 
