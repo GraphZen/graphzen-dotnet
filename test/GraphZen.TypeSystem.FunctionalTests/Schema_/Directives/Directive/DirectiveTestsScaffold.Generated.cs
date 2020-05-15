8a70e4e8a48563147462c0b7715fb58d02e67ae3
Craig Smitham <craig@graphzen.com>
2020-05-15 12:12:09 -0500

    saving changes before code-gen

--- test/GraphZen.TypeSystem.FunctionalTests/Schema_/Directives/Directive/DirectiveTestsScaffold.Generated.cs
+++ test/GraphZen.TypeSystem.FunctionalTests/Schema_/Directives/Directive/DirectiveTestsScaffold.Generated.cs
@@ -1,30 +1,29 @@
 #nullable enable
 
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using GraphZen.Infrastructure;
 using JetBrains.Annotations;
-
 using Xunit;
 using static GraphZen.TypeSystem.FunctionalTests.Specs.TypeSystemSpecs;
+
 // ReSharper disable All
-namespace GraphZen.TypeSystem.FunctionalTests.Schema_.Directives.Directive {
-[NoReorder]
-public abstract  class DirectiveTests {
+namespace GraphZen.TypeSystem.FunctionalTests.Schema_.Directives.Directive
+{
+    [NoReorder]
+    public abstract class DirectiveTests
+    {
+        [Spec(nameof(SdlSpec.item_can_be_defined_by_sdl))]
+        [Fact(Skip = "TODO")]
+        public void item_can_be_defined_by_sdl_()
+        {
+            // var schema = Schema.Create(_ => { });
+        }
+    }
 
-[Spec(nameof(SdlSpec.item_can_be_defined_by_sdl))]
-[Fact(Skip="TODO")]
-public void item_can_be_defined_by_sdl_() {
-    // var schema = Schema.Create(_ => { });
-}
-
-
-}
 // Move DirectiveTests into a separate file to start writing tests
-[NoReorder] 
-public  class DirectiveTestsScaffold {
-}
+    [NoReorder]
+    public class DirectiveTestsScaffold
+    {
+    }
 }
 // Source Hash Code: 4524757103880778831
