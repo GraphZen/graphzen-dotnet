1729a480e8ad43ae3767423d3381244b9e3e7771
Craig Smitham <craig@graphzen.com>
2020-06-27 13:05:01 -0500

    saving changes before code-gen

--- test/GraphZen.TypeSystem.FunctionalTests/Schema_/DirectiveAnnotations/DirectiveAnnotationTestsScaffold.Generated.cs
+++ test/GraphZen.TypeSystem.FunctionalTests/Schema_/DirectiveAnnotations/DirectiveAnnotationTestsScaffold.Generated.cs
@@ -1,29 +1,24 @@
 #nullable enable
 
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
+// ReSharper disable All
 using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
 using GraphZen.Infrastructure;
 using JetBrains.Annotations;
-
-// ReSharper disable All
-using FluentAssertions;
 using Xunit;
 using static GraphZen.TypeSystem.FunctionalTests.Specs.TypeSystemSpecs.DirectiveAnnotationSpecs;
 
-namespace GraphZen.TypeSystem.FunctionalTests.Schema_.DirectiveAnnotations {
-[NoReorder]
-public abstract  class DirectiveAnnotationTestsScaffold {
-
-
-[Spec(nameof(directive_annotations_are_removed_when_directive_is_ignored))]
-[Fact(Skip="TODO")]
-public void directive_annotations_are_removed_when_directive_is_ignored_() {
-    // var schema = Schema.Create(_ => { });
-}
-
-
-}
+namespace GraphZen.TypeSystem.FunctionalTests.Schema_.DirectiveAnnotations
+{
+    [NoReorder]
+    public abstract class DirectiveAnnotationTestsScaffold
+    {
+        [Spec(nameof(directive_annotations_are_removed_when_directive_is_ignored))]
+        [Fact(Skip = "TODO")]
+        public void directive_annotations_are_removed_when_directive_is_ignored_()
+        {
+            // var schema = Schema.Create(_ => { });
+        }
+    }
 }
 // Source Hash Code: 8311931708836856797
